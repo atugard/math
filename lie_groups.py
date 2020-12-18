6c7ba95a2ed3d7656c00da6e5cd66f1a5ed2477b
David Draguta <daviddraguta@gmail.com>
2020-12-18 13:34:55 -0500

    First commie-mit

---
+++ lie_groups.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import math 
+import matplotlib.pyplot as plt 
+#matplotlib inline 
+
+x,y = np.meshgrid(np.linspace(-5,5,20), np.linspace(-5,5,20))
+
+#u=1 
+#v=-1
+mElliptical = exp(np.array([exp(4
+u=
+#plt.quiver(x,y,u,v)
+#plt.show() 
