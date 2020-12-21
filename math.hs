fb7f36a4b96deb4e68394fcf344a6df094d73960
David Draguta <daviddraguta@gmail.com>
2020-12-20 22:05:54 -0500

    Implement Euler's totient function.

--- math.hs
+++ math.hs
@@ -1,3 +1,5 @@
+import Data.Maybe 
+
 _gcdTrail :: Int -> Int -> [[Int]] -> [[Int]]
 _gcdTrail 0 _ res = res 
 _gcdTrail _ 0 res = res 
@@ -16,6 +18,7 @@ gcdTrail x y
   | otherwise = _gcdTrail x y [[x,0,y]]
 
 _backTrail :: [[Int]] -> [[Int]] 
+_backTrail [] = [] 
 _backTrail (x:xs) 
   | null xs   = xs
   | otherwise =
@@ -47,4 +50,34 @@ _gcdLC (x:xs)
 --Returns a pair (g, [u,x,v,y]) such that g = gcd x y and u*x + v*y = g
 gcdLC :: Int -> Int -> (Int, [Int])
 gcdLC x y = (gcd x y, _gcdLC $ backTrail x y)
+
+--Given a,n find k such that ak=n 
+inverseModuloN :: Int -> Int -> Maybe Int  
+inverseModuloN _ 0 = Nothing 
+inverseModuloN a n 
+  | a > n     = inverseModuloN (a `mod` n) n  
+  | a == 1    = Just 1 
+  | otherwise = let _d = gcdLC a n
+                    g  = fst _d 
+                    d  = snd _d
+                 in if g /= 1  --waste to do all of that work and then finally check this condition... I should rewrite this...
+                       then Nothing 
+                       else if d !! 1 == n 
+                               then Just $ (d !! 2) `mod` n 
+                               else Just $ (d !! 0) `mod` n
+--Counts number of coprime elements 
+_totient :: Int -> Int -> Int -> Int 
+_totient total a n 
+  | a == n = total 
+  | otherwise = let d = inverseModuloN a n 
+                 in if isNothing d 
+                       then _totient total (a+1) n 
+                       else _totient (total+1) (a+1) n
+
+totient :: Int -> Int 
+totient 0 = 1 
+totient 1 = 1 
+totient n 
+  | n < 0     = error $ "totient takes one non negative argument of type Integer. You gave: " ++ (show n)
+  | otherwise =  _totient 0 0 n
 
